e64bb6dc02f3dfbdb299943cdd6750e069f522f4
vincentliu130 <she4869@hotmail.com>
2016-08-03 19:02:12 -0400

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 		'static_pages#home'	
-  get 		'/help',	to: 'static_pages#help'
+  get 		'/help',	  to: 'static_pages#help'
   get 		'/about', 	to: 'static_pages#about'
   get 		'/contact', to: 'static_pages#contact'
   get 		'/signup', 	to: 'users#new'
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   delete 	'/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:edit]
 end
