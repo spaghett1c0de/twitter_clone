1f43d020a4363ab054e4db2f7589f43ec47284c4
vincentliu130 <she4869@hotmail.com>
2016-08-12 17:29:20 -0400

    title bug fixed

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 	test "full title helper" do
-		assert_equal full_title, 			 "Ruby on Rails Tutorial Sample App"
-		assert_equal full_title("Help"), 	 "Help | Ruby on Rails Tutorial Sample App"
-		assert_equal full_title("About"), 	 "About | Ruby on Rails Tutorial Sample App"
-		assert_equal full_title("Contact"),  "Contact | Ruby on Rails Tutorial Sample App"
+		assert_equal full_title, 			 "Insider Communication"
+		assert_equal full_title("Help"), 	 "Help | Insider Communication"
+		assert_equal full_title("About"), 	 "About | Insider Communication"
+		assert_equal full_title("Contact"),  "Contact | Insider Communication"
 	end
 end
